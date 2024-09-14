16ea5f39a6c89b64b7d3a01cc36eb2fce773e587
Miguel Angel Feo Merchan <mfeomerchan@gmail.com>
2024-09-14 15:45:28 -0500

    Sube archivos de control de estudiantes

---
+++ sistemaedu/src/main/java/com/example/sistemaedu/bd/EstudianteORM.java
@@ -0,0 +1,46 @@
+package com.example.sistemaedu.bd;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "estudiante")
+@Entity
+@Data
+@NoArgsConstructor
+public class EstudianteORM {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String genero;
+
+    @Column
+    private Integer edad;
+    @Column
+    private String carrera;
+
+    @Column
+    private String email;
+
+    @Column
+    private Integer semestre;
+
+    @Column
+    private float promedio;
+
+    public EstudianteORM( String nombre, String genero, Integer edad, String carrera, String email, Integer semestre, float promedio) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.email = email;
+        this.semestre = semestre;
+        this.promedio = promedio;
+    }
+
+}
---
+++ sistemaedu/src/main/java/com/example/sistemaedu/controller/EstudianteController.java
@@ -0,0 +1,63 @@
+package com.example.sistemaedu.controller;
+
+import com.example.sistemaedu.bd.EstudianteJPA;
+import com.example.sistemaedu.bd.EstudianteORM;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin("http://localhost:3000")
+public class EstudianteController {
+
+    private EstudianteJPA estudianteJPA;
+
+    List<EstudianteDTO> estudiantes = new ArrayList<>();
+
+    @GetMapping(path = "/saludo")
+    public String saludar() {
+        return "Hola mundo";
+    }
+
+
+    @PostMapping(path = "/estudiante")
+    public String guardarEstudiante(@RequestBody EstudianteDTO estudiante) {
+        estudiantes.add(estudiante);
+        estudianteJPA.save(new EstudianteORM(estudiante.nombre(), estudiante.genero(), estudiante.edad(),estudiante.carrera(), estudiante.email(), estudiante.semestre(), estudiante.promedio()));
+        return "Estudiante guardado";
+    }
+    @GetMapping(path = "/estudiantes-bd")
+    public List<EstudianteORM> obtenerEstudiantesBD() {
+        return estudianteJPA.findAll();
+    }
+
+    @PutMapping(path = "/estudiante/{id}")
+    public String actualizarEstudiante(@PathVariable Long id, @RequestBody EstudianteDTO estudiante){
+        EstudianteORM estudianteExistente = estudianteJPA.findById(id).orElse(null);
+        if (estudianteExistente != null){
+            estudianteExistente.setNombre(estudiante.nombre());
+            estudianteExistente.setGenero(estudiante.genero());
+            estudianteExistente.setEdad(estudiante.edad());
+            estudianteExistente.setCarrera(estudiante.carrera());
+            estudianteExistente.setEmail(estudiante.email());
+            estudianteExistente.setSemestre(estudiante.semestre());
+            estudianteExistente.setPromedio(estudiante.promedio());
+
+            // Guardar los cambios en la base de datos
+            estudianteJPA.save(estudianteExistente);
+
+            return "Estudiante actualizado correctamente";
+        } else {
+            return "Estudiante no encontrado";
+        }
+    }
+    @DeleteMapping(path = "/estudianteEliminado/{id}")
+    public String eliminarEstudiante(@PathVariable Long id) {
+        estudianteJPA.deleteById(id);
+        return "Estudiante eliminado";
+    }
+
+}
