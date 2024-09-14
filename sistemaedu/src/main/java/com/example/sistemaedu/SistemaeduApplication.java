fbaf2722167eba78876ac077087dcaab1b967665
Ricardo Andrés Cortés Coronell <116464125+Rickay1121@users.noreply.github.com>
2024-09-14 14:15:44 -0500

    Añade Spring Intializer

---
+++ sistemaedu/src/main/java/com/example/sistemaedu/SistemaeduApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaeduApplication.class, args);
+	}
+
+}
---
+++ sistemaedu/src/test/java/com/example/sistemaedu/SistemaeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
