0564dfdec33e57f53c8eeb90224785ee61dc1f38
Miguel Angel Feo Merchan <mfeomerchan@gmail.com>
2024-09-22 17:26:41 -0500

    Crea tests para actualizar

--- sistemaedu/src/test/java/com/example/sistemaedu/EstudianteServiceTest.java
+++ sistemaedu/src/test/java/com/example/sistemaedu/EstudianteServiceTest.java
@@ -85,4 +85,67 @@ class EstudianteServiceTest {
         Assertions.assertTrue(resultado);
         Mockito.verify(estudianteJPA).save(Mockito.any());
     }
+
+    @Test
+    void Given_promedioInvalidoMenorA0_When_actualizarEstudiante_Then_throwIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.actualizarEstudiante(10L, "Plarci Rodriguez", "Masculino", 20, "Ingenieria de Sistemas", "yarci@gmail.com", 5, -1.0f)
+        );
+    }
+
+    @Test
+    void Given_promedioInvalidoMayorA5_When_actualizarEstudiante_Then_throwIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.actualizarEstudiante(10L, "Plarci Rodriguez", "Masculino", 20, "Ingenieria de Sistemas", "yarci@gmail.com", 5, 6.0f)
+        );
+    }
+
+    @Test
+    void Given_edadInvalidoMenorA0_When_actualizarEstudiante_Then_throwIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.actualizarEstudiante(10L, "Plarci Rodriguez", "Masculino", -20, "Ingenieria de Sistemas", "yarci@gmail.com", 5, 5.0f)
+        );
+    }
+
+    @Test
+    void Given_edadInvalidoMayorA100_When_actualizarEstudiante_Then_throwIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.actualizarEstudiante(10L, "Plarci Rodriguez", "Masculino", 101, "Ingenieria de Sistemas", "yarci@gmail.com", 5, 5.0f)
+        );
+    }
+
+    @Test
+    void Given_semestreInvalidoMenorA0_When_actualizarEstudiante_Then_throwIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.actualizarEstudiante(10L, "Plarci Rodriguez", "Masculino", 20, "Ingenieria de Sistemas", "yarci@gmail.com", -1, 5.0f)
+        );
+    }
+
+    @Test
+    void Given_semestreInvalidoMayorA14_When_actualizarEstudiante_Then_throwIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.actualizarEstudiante(10L, "Plarci Rodriguez", "Masculino", 20, "Ingenieria de Sistemas", "yarci@gmail.com", 15, 5.0f)
+        );
+    }
+
+    @Test
+    void Given_emailExistente_When_actualizarEstudiante_Then_throwIllegalArgumentException() {
+        // Arrange
+        String emailExistente = "pedro.gomez@correo.com";
+        when(estudianteJPA.findByEmail(emailExistente)).thenReturn(Optional.of(new EstudianteORM()));
+
+        // Act & Assert
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> service.actualizarEstudiante(10L, "Plarci Rodriguez", "Masculino", 19, "Ingenieria de Sistemas", "pedro.gomez@correo.com", 8, 5.0f)
+        );
+    }
+
+    @Test
+    void When_actualizarEstudiante_Then_returnTrue() {
+        when(estudianteJPA.findById(10L)).thenReturn(Optional.of(new EstudianteORM()));
+        boolean resultado = service.actualizarEstudiante(10L, "Plarci Rodriguez", "Masculino", 18, "Ingenieria de Sistemas", "yarci@gmail.com", 12, 5.0f);
+        Assertions.assertTrue(resultado);
+        Mockito.verify(estudianteJPA).save(Mockito.any());
+    }
 }
+
